9ab296caaa7bc164d5da69252feb7ba4a7e3179f
nyc4m <baptiste.prunot@ik.me>
2021-10-28 21:21:37 +0200

    start timer from 0

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { timer, BehaviorSubject, Observable } from 'rxjs';
 import { map, take, switchMap, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-timer',
@@ -8,15 +9,15 @@ import { map, take, switchMap, tap } from 'rxjs/operators';
   styleUrls: ['./timer.component.sass'],
 })
 export class TimerComponent {
-  private readonly timeToWait = 45;
+  readonly timeToWait = environment.timeToWait;
 
   started = false;
 
-  clock$ = timer(1000, 1000).pipe(map((s) => s + 1));
+  overallClock$ = timer(0, 1000).pipe(map((s) => s));
 
-  private timer45seconds$ = this.clock$.pipe(take(this.timeToWait));
+  private subTimer$ = this.overallClock$.pipe(take(this.timeToWait));
 
-  private timer$ = new BehaviorSubject(this.timer45seconds$);
+  private timer$ = new BehaviorSubject(this.subTimer$);
 
   timeToPoor$ = new BehaviorSubject(false);
 
@@ -24,14 +25,14 @@ export class TimerComponent {
 
   onReset() {
     this.timeToPoor$.next(false);
-    this.timer$.next(this.timer45seconds$.pipe(this.notifyWhenTimeIsDone));
+    this.timer$.next(this.subTimer$.pipe(this.notifyWhenTimeIsDone));
   }
 
   private readonly notifyWhenTimeIsDone = (obs: Observable<number>) => {
     const { timeToWait, timeToPoor$ } = this;
     return obs.pipe(
       tap((t) => {
-        if (t >= timeToWait) {
+        if (t >= timeToWait - 1) {
           timeToPoor$.next(true);
         }
       })
@@ -40,10 +41,11 @@ export class TimerComponent {
 
   onStart() {
     this.started = true;
-    this.timer$.next(this.timer45seconds$.pipe(this.notifyWhenTimeIsDone));
+    this.timer$.next(this.subTimer$.pipe(this.notifyWhenTimeIsDone));
   }
 
   onStop() {
     this.started = false;
+    this.timeToPoor$.next(false);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  timeToWait: 46,
 };
