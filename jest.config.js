74daca2b172b7ed563b59d627fa67dd5cb3557ae
nyc4m <baptiste.prunot@ik.me>
2021-10-29 17:48:10 +0200

    add first tests and styling

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
   transform: {
        '^.+\\.vue$': 'vue3-jest',
   },
-  moduleFileExtensions: ['json', 'js', 'jsx', 'ts', 'tsx', 'vue']
+  moduleFileExtensions: ['json', 'js', 'jsx', 'ts', 'tsx', 'vue'],
+  setupFilesAfterEnv: ['./src/jest-setup.ts']
 };
--- src/components/CoffeeTimer.vue
+++ src/components/CoffeeTimer.vue
@@ -1,13 +1,45 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
+const started = ref(false)
 
 </script>
 
-<template></template>
+<template>
+    <div v-if="started" class="timer">
+        <p>Started</p>
+        <button class="timer--button-stop" @click="started = false">Stop</button>
+    </div>
+    <div v-else class="timer">
+        <button class="timer--button-start" @click="() => started = true">Start Timer</button>
+    </div>
+</template>
 
-<style scoped>
+<style scoped lang="sass">
+
+@mixin button
+    font-size: 1.2rem
+    padding: 0.3rem
+    background-color: white
+    border-color: lightgrey
+    color: lightgrey
+    border-radius: 6px
+    border-style: solid
+    border-width: 2px
+    &:hover
+        background-color: #f4f4f4
+    &:active
+        background-color: #f6f6f6
+
+
+.timer
+    display: flex
+    flex-direction: column
+    align-items: center
+    &--button-start
+        @include button
+    &--button-stop
+        @include button
+        border-color: pink
+        color: pink
 </style>
