4031aafc24627d12386cb5a639c84e8ff447b9e9
nyc4m <baptiste.prunot@ik.me>
2021-10-29 13:47:02 +0200

    bye angular

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-// jest.config.js
-module.exports = {
-    preset: 'jest-preset-angular',
-    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-    transformIgnorePatterns: ['node_modules/(?!@ngrx|angular2-ui-switch|ng-dynamic)']
-};
